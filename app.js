ab659a5739bc36977a90cdf10560c52122bfd48c
mitmarcopardo <marco.pardozf@gmail.com>
2023-08-05 01:23:22 -0600

    8080 port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT ?? 3001;
+const port = process.env.PORT ?? 8080;
 
 app.use(express.static('public'))
 app.use(cors
